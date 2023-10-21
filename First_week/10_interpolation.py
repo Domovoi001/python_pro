a0ede30c13ba197148386837b49af0a21a1de6eb
Leks9601~ <jiganskie007@gmail.com>
2023-10-21 15:07:49 +0500

    Добавил решение 9 и 10 упражнения

---
+++ First_week/10_interpolation.py
@@ -0,0 +1,7 @@
+# BEGIN (write your solution here)
+stark = "Arya"
+text = f'''Do you want to eat, {stark}?
+Yes, I'm hungry, mom.
+'''
+print(text)
+# END
---
+++ First_week/9_naming.py
@@ -0,0 +1,7 @@
+king = 'King Balon the 6th'
+
+# BEGIN (write your solution here)
+count_castles = 6
+count_rooms_in_castle = 17
+print(king + ' has ' + str(count_castles * count_rooms_in_castle) + ' rooms.')
+# END
